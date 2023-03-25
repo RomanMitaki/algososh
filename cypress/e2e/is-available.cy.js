aa8e8b1be89f3f7a2b865f66e9058230a078a891
RomanMitaki <r.mitaki@yandex.ru>
2023-03-25 12:15:18 +0300

    add e2e-test is-available

---
+++ cypress/e2e/is-available.cy.js
@@ -0,0 +1,11 @@
+describe('Application is available', () => {
+    context('macbook 16 resolution', () => {
+        beforeEach(() => {
+            cy.viewport(1536, 960)
+        })
+
+        it('should be available on localhost:3000', () => {
+            cy.visit('http://localhost:3000')
+        })
+    })
+})
